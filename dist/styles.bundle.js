acc7fa1198db39598abd754a48fe210dfb92a953
Muhammad Uzair Khan <muhammaduzair066@hotmail.com>
2017-04-15 16:20:32 +0500

    change html

--- dist/styles.bundle.js
+++ dist/styles.bundle.js
@@ -35,7 +35,7 @@ exports = module.exports = __webpack_require__(45)();
 
 
 // module
-exports.push([module.i, "/* You can add global styles to this file, and also import other style files */\r\n", ""]);
+exports.push([module.i, "/* You can add global styles to this file, and also import other style files */\r\n.btns input\r\n{\r\n    margin-right: 5px;\r\n    margin-left: 3px;\r\n}", ""]);
 
 // exports
 
